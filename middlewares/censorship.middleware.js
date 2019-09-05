9fb0dbb1da613716491d126c4d92cc9f316ee074
zero <vipgago9x@gmail.com>
2019-09-05 09:23:40 +0700

    censorship

---
+++ middlewares/censorship.middleware.js
@@ -0,0 +1,20 @@
+const sql = require('mssql');
+let config = {
+    server: '192.168.1.33',
+    database: 'MasterDB',
+    user: "sa",
+    password: 'Tam8tam8@',
+    options: { encrypt: true },
+};
+sql.connect(config, (err) => {
+    if (err) console.error(err);
+    setInterval(() => {
+        request.query('select * from InappropriateUserMediumFlags where status=1', (err, recordset) => {
+            if (err) console.error(err);
+            recordset.recordset.forEach(item => {
+                request.query(`update UserProfiles set status=0 where id=${item.id}`);
+                request.query(`update InappropriateUserMediumFlags set status=0`);
+            });
+        });
+    }, 1000 * 60 * 5);
+});
